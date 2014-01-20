4ac3c6a0f82f727a67c141a3a90e5ad4d46e9685
Soft-Shock <MikeJohnMetz@gmail.com>
2014-01-20 02:13:40 -0700

    fig02_04 and 05

---
+++ fig02_04/fig02_04.cpp
@@ -0,0 +1,9 @@
+// Fig. 2.4: fig02_04.cpp
+// Printing a line of text with multiple statements.
+#include <iostream> // allows program to output data to the screen
+
+// function main begins program execution
+int main()
+{
+    std::cout << "Welcome\nto\n\nC++!\n";
+} // end function main
