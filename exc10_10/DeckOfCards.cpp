bad8ba3e2258e92056b9e0a066180d5e440c298f
Soft-Shock <MikeJohnMetz@gmail.com>
2014-02-18 23:50:10 -0700

    Template of Problem

---
+++ exc10_10/DeckOfCards.cpp
@@ -0,0 +1,11 @@
+#include "DeckOfCards.h"
+
+
+DeckOfCards::DeckOfCards()
+{
+}
+
+
+DeckOfCards::~DeckOfCards()
+{
+}
---
+++ exc10_10/DeckOfCards.h
@@ -0,0 +1,8 @@
+#pragma once
+class DeckOfCards
+{
+public:
+    DeckOfCards();
+    ~DeckOfCards();
+};
+
