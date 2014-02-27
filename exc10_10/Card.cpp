41870b9c4a258ed0971223f67930b9bf4ce97765
Soft-Shock <MikeJohnMetz@gmail.com>
2014-02-26 23:47:48 -0700

    Modified Card Class

--- exc10_10/Card.cpp
+++ exc10_10/Card.cpp
@@ -1,10 +1,30 @@
 #include "Card.h"
+#include <string>
+#include <vector>
 
+using namespace std;
+/*
+    Data Members
+    int mcFaceCd;
+    int mcSuitCd;
+    static const vector<int> mdFaces( 13 );
+    static vector<int> mdSuits( 4 );
+*/
+const vector<string> Card::mvdFaces(13);
+const vector<string> Card::mvdFaces =
+    { "2", "3", "4", "5",
+    "6", "7", "8", "9",
+    "10", "Jack", "Queen", "King", "Ace" };
+const vector<int> mvdSuits =
+    { "Diamonds", "Clubs", "Hearts", "Spades" };
 
-Card::Card()
+Card::Card(int mFace, int mSuit)
 {
 }
 
+string Card::toString() const
+{
+}
 
 Card::~Card()
 {
--- exc10_10/Card.h
+++ exc10_10/Card.h
@@ -1,8 +1,18 @@
-#pragma once
+#ifndef CARD_H
+#define CARD_H
+#include <string>
+#include <vector>
+
 class Card
 {
 public:
-    Card();
+    Card::Card(int mFace, int mSuit);
     ~Card();
+private:
+    int mcFace;
+    int mcSuit;
+    const static std::vector<std::string> mvdFaces;
+    const static std::vector<std::string> mvdSuits;
 };
 
+#endif
